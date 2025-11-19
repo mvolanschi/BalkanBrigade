3e82c2d3373d4e3287691563492cd53289086b93
Andrei Radu Milicin <andrei.milicin@gmail.com>
2025-11-19 20:37:44 +0100

    pula

--- backend/src/main.py
+++ backend/src/main.py
@@ -27,7 +27,7 @@ app = FastAPI(title="Interview Practice Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("CORS_ORIGIN", "*")],
+    allow_origins=[os.getenv("CORS_ORIGIN", "http://localhost:3000/")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
